01cbd05279d5c8d6a9b0f7bcccc41cd6718cab74
Bruno Brito <bruno.brito@clickbus.com>
2020-03-02 14:59:41 -0300

    Start project

---
+++ app/src/main/java/com/br/agenda/MainActivity.java
@@ -0,0 +1,14 @@
+package com.br.agenda;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+    }
+}
